bf8ad9bbd2017de8b1b84b09421d6e5d8c1d0846
sraser <38656717+sraser@users.noreply.github.com>
2022-02-14 20:51:35 +0900

    Update biteth00.py

--- biteth00.py
+++ biteth00.py
@@ -1,21 +1,20 @@
 import time
 import pyupbit
-import datetime
-import numpy as np
 import telegram
 
 access = ""
 secret = ""
+bot = telegram.Bot(token='')
+chat_id =
 
-def get_target_price(ticker, k):
-    df = pyupbit.get_ohlcv(ticker, interval="day", count=2)
-    target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * k
-    return target_price
-
-def get_start_time(ticker):
-    df = pyupbit.get_ohlcv(ticker, interval="day", count=1)
-    start_time = df.index[0]
-    return start_time
+inter = "minute240"
+def get_ma(ticker):
+    dfma = pyupbit.get_ohlcv(ticker, interval=inter, count=22, period=1)
+    ma5 = dfma['close'].rolling(5).mean().iloc[-1]
+    ma20 = dfma['close'].rolling(20).mean().iloc[-1]
+    ma5p = dfma['close'].rolling(5).mean().iloc[-2]
+    ma20p = dfma['close'].rolling(20).mean().iloc[-2]
+    return ma5, ma20, ma5p, ma20p
 
 def get_balance(ticker):
     balances = upbit.get_balances()
@@ -30,104 +29,93 @@ def get_balance(ticker):
 def get_current_price(ticker):
     return pyupbit.get_current_price(ticker)
 
-def get_bestk(ticker):
-    bestk = 0
-    bestror = 0
-    df = pyupbit.get_ohlcv(ticker, count=7, period=1)
-    for k in np.arange(0.3, 1.0, 0.05):
-        df['range'] = (df['high'] - df['low']) * k
-        df['target'] = df['open'] + df['range'].shift(1)
-        df['ror'] = np.where(df['high'] > df['target'],
-                             df['close'] / df['target'],
-                             1)
-        df['hpr'] = df['ror'].cumprod()
-        if df.iloc[-1]['hpr'] > bestror:
-            bestk = k
-            bestror = df.iloc[-1]['hpr']
-    return bestk, bestror
-
 upbit = pyupbit.Upbit(access, secret)
 print("autotrade start")
 
 btc_state = 0
 eth_state = 0
+btc_buy_price = 0
+btc_sell_price = 0
+eth_buy_price = 0
+eth_sell_price = 0
 
 btc_ini = get_balance("BTC")
 eth_ini = get_balance("ETH")
-
+time.sleep(0.2)
+btcma5, btcma20, btcma5p, btcma20p = get_ma("KRW-BTC")
+time.sleep(0.2)
+ethma5, ethma20, ethma5p, ethma20p = get_ma("KRW-ETH")
+bot.sendMessage(chat_id=chat_id, text="재접속")
 if btc_ini != 0 :
     btc_state = 1
+    btc_buy_price = btc_ini
 if eth_ini != 0 :
     eth_state = 1
+    eth_buy_price = eth_ini
+if btcma5 >= btcma20 and btc_state == 0:
+    btc_state = 1
+if ethma5 >= ethma20 and btc_state == 0:
+    eth_state = 1
+
+
 
 while True:
     try:
-        now = datetime.datetime.now()
-        start_time = get_start_time("KRW-BTC")
-        end_time = start_time + datetime.timedelta(days=1)
-        bot = telegram.Bot(token='')
-        chat_id = 1636335355
-
-        bestcalkBTC, bestcalrorBTC = get_bestk("KRW-BTC")
-        target_price = get_target_price("KRW-BTC", bestcalkBTC)
-        current_price = get_current_price("KRW-BTC")
-        btcfee = 5000 / target_price
+        current_priceBTC = get_current_price("KRW-BTC")
+        btcfee = 5000 / current_priceBTC
         btc = get_balance("BTC")
+        btcma5, btcma20, btcma5p, btcma20p = get_ma("KRW-BTC")
         time.sleep(0.1)
 
-        bestcalkETH, bestcalrorETH  = get_bestk("KRW-ETH")
-        target_priceETH = get_target_price("KRW-ETH", bestcalkETH)
         current_priceETH = get_current_price("KRW-ETH")
-        ethfee = 5000 / target_priceETH
+        ethfee = 5000 / current_priceETH
         eth = get_balance("ETH")
+        ethma5, ethma20, ethma5p, ethma20p = get_ma("KRW-ETH")
         time.sleep(0.1)
 
         krw = get_balance("KRW")
 
-        if start_time < now < start_time + datetime.timedelta(seconds=15):
-            btcratio = (target_price - current_price) / current_price * 100
-            ethratio = (target_priceETH - current_priceETH) / current_priceETH * 100
-            msgBTC = "BTC : k = %.2f , 목표 = %d , \n7일최대 = %.4f %%, 요구상승 : %.2f %%" % (bestcalkBTC, target_price, bestcalrorBTC*100, btcratio)
-            msgETH = "ETH : k = %.2f , 목표 = %d , \n7일최대 = %.4f %%, 요구상승 : %.2f %%" % (bestcalkETH, target_priceETH, bestcalrorETH*100, ethratio)
-            msgKRW = "오늘의 시드머니 = %d 원" % (krw)
-            bot.sendMessage(chat_id=chat_id, text=msgBTC)
-            bot.sendMessage(chat_id=chat_id, text=msgETH)
-            bot.sendMessage(chat_id=chat_id, text=msgKRW)
-            time.sleep(15)
-
-        if start_time + datetime.timedelta(seconds=15) < now < end_time - datetime.timedelta(seconds=30):
-            if target_price < current_price:
-                btcbuycall = 0
-                if eth > ethfee :
-                    btcbuycall = btcbuycall + 1
-                if krw > 5000 and btc < btcfee:
-                    upbit.buy_market_order("KRW-BTC", krw / (2-btcbuycall) * 0.9995)
-                    btc_state = 1
-                time.sleep(0.2)
-            if target_priceETH < current_priceETH:
-                ethbuycall = 0
-                if btc > btcfee :
-                    ethbuycall = ethbuycall + 1
-                if krw > 5000 and eth < ethfee:
-                    upbit.buy_market_order("KRW-ETH", krw / (2-ethbuycall) * 0.9995)
-                    eth_state = 1
-                time.sleep(0.2)
-            time.sleep(30)
-
-        else:
-            if btc_state == 1:
-                upbit.sell_market_order("KRW-BTC", btc)
-                btc_state = 0
-                cpt = current_price / target_price * 100 - 100
-                msg = "오늘의 BTC 수익률 = %.2f %%" % (cpt)
+        if btcma5 >= btcma20 and btcma5p < btcma20p and btc_state == 0:
+            btcbuycall = 0
+            if eth > ethfee :
+                btcbuycall = btcbuycall + 1
+            if krw > 5000 and btc < btcfee:
+                upbit.buy_market_order("KRW-BTC", krw / (2-btcbuycall) * 0.9995)
+                btc_buy_price = current_priceBTC
+                btc_state = 1
+                msg = "BTC 매수 = %.2f" % (btc_buy_price)
                 bot.sendMessage(chat_id=chat_id, text=msg)
-            if eth_state == 1:
-                upbit.sell_market_order("KRW-ETH", eth)
-                cpt = current_priceETH / target_priceETH * 100 - 100
-                msg = "오늘의 ETH 수익률 = %.2f %%" % (cpt)
-                eth_state = 0
+            time.sleep(0.2)
+
+        if ethma5 >= ethma20 and ethma5p < ethma20p and eth_state == 0:
+            ethbuycall = 0
+            if btc > btcfee :
+                ethbuycall = ethbuycall + 1
+            if krw > 5000 and eth < ethfee:
+                upbit.buy_market_order("KRW-ETH", krw / (2-ethbuycall) * 0.9995)
+                eth_buy_price = current_priceETH
+                eth_state = 1
+                msg = "ETH 매수 = %.2f" % (eth_buy_price)
                 bot.sendMessage(chat_id=chat_id, text=msg)
-        time.sleep(1)
+            time.sleep(0.2)
+
+        if btcma5 <= btcma20 and btcma5p > btcma20p and btc_state == 1:
+            upbit.sell_market_order("KRW-BTC", btc)
+            btc_state = 0
+            btc_sell_price = current_priceBTC
+            cpt = (btc_buy_price - btc_sell_price) / btc_buy_price + 1
+            msg = "BTC 매도 %.2f , 수익률 = %.2f %%" % (btc_sell_price, cpt)
+            bot.sendMessage(chat_id=chat_id, text=msg)
+
+        if ethma5 <= ethma20 and ethma5p > ethma20p and eth_state == 1:
+            upbit.sell_market_order("KRW-ETH", eth)
+            eth_sell_price = current_priceETH
+            cpt = (eth_buy_price - eth_sell_price) / eth_buy_price + 1
+            msg = "BTC 매도 %.2f , 수익률 = %.2f %%" % (eth_sell_price, cpt)
+            eth_state = 0
+            bot.sendMessage(chat_id=chat_id, text=msg)
+        time.sleep(60)
+
     except Exception as e:
         print(e)
         time.sleep(1)
